d5939e1e73e4ccba2d1a980cb9fc40264f4d6259
Nguyễn Thanh Hung <nguyenthanhhung24102000@gmail.com>
2019-09-24 15:32:13 +0700

    Exercise03

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            Hline2(10, '*'); Console.WriteLine();
+            Hline2(10, 'a'); Console.WriteLine();
+            Hline2(10, 'b'); Console.WriteLine();
+        }
+        static void Hline2 (int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(c);
+        }
+        static void Vline(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(c); bv .
+        }
+    }
+}
